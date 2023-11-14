49f6b7b7e32af7a56664aeb184d54a54267a126a
christianlwhite <clwhite@me.com>
2023-11-14 13:53:24 -0500

    google map api, calendar ui, js file, minor changes to text and appearance

---
+++ app.js
@@ -0,0 +1,28 @@
+////function init() {
+///    var mapOptions = {
+      ///  center: new google.maps.LatLng(38.27283778412796, -85.70843950344207), mapTypeId: google.maps.MapTypeId.ROADMAP, zoom: 13
+    ///};
+    ///var venueMap;
+    ///venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+///}
+
+///function loadScript() {
+    ///var script = document.createElement('script');
+    ///script.src = 'http://maps.googleleapis.com/maps/api/js?
+                        ///sensor=false&callback=init';
+    ///document.body.appendChild(script);
+///}
+
+///window.onload = loadScript;
+
+document.getElementById("https://storage.googleapis.com/maps-solutions-4fwv6e5z9f/commutes/b80e/commutes.html").style.textAlign = "center";
+
+////black mountain api////https://api.open-meteo.com/v1/forecast?latitude=36.91&longitude=-82.89&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1
+
+////woodford county (bluegrass)////https://api.open-meteo.com/v1/forecast?latitude=38.11&longitude=-84.69&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1
+
+////farmland western ky////https://api.open-meteo.com/v1/forecast?latitude=36.88&longitude=-86.29&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1
+
+////cumberland falls ky/////https://api.open-meteo.com/v1/forecast?latitude=36.84&longitude=-84.34&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FNew_York&forecast_days=1
+
+////louisville////https://api.open-meteo.com/v1/forecast?latitude=38.25&longitude=-85.75&hourly=temperature_2m&forecast_days=1
